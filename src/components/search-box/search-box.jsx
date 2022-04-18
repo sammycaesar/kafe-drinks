de847ab7fd3258058b66b1d15c4755686b9c3fc6
Samantha Caesar <samantha.caesar@bigpond.com>
2022-04-18 14:09:24 +0200

    updates to front end

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -3,10 +3,14 @@ import React from "react";
 import "./search-box.styles.css";
 
 export const SearchBox = ({ placeholder, handleChange }) => (
-  <input
-    className='search'
-    type='search'
-    placeholder={placeholder}
-    onChange={handleChange}
-  />
+  <div className='search-container'>
+    <div className='search-box'>
+      <input
+        className='search'
+        type='search'
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    </div>
+  </div>
 );
